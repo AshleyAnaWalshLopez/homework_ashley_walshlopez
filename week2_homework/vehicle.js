5322236b1d9baff5cb33ceb8846d3e574e896323
AshleyAnaWalshLopez <ashleyana.walsh@gmail.com>
2018-09-24 15:40:12 -0700

    week2 hw

---
+++ week2_homework/vehicle.js
@@ -0,0 +1,35 @@
+/*
+* You get to give yourself an awesome vehicle
+* It is an object
+* Make it badass
+* Be sure to give it the following:
+*   - a name that is a string
+*   - a distanceToEmpty that is a number of kilometers
+ it can travel
+*   - a speed that is a number and represents the number
+of km/hr your vehicle can travel
+*   - a currentLocation
+*   - a honkHorn method that returns a string of the
+sound that your vehicle makes when you honk the horn
+*/
+
+var helicopter = {
+  name: "Cobra",
+  color:"Death Grey"
+  distanceToEmpty: 322,
+  speed: 130,
+  currentLocation: "Flight Line",
+  acceleration: function (){
+      return "I can travel" + this.distanceToEmpty;
+  }
+
+
+    missile:{
+      name: "Hellfire",
+      /*honkHorn mechanism  */
+      sound: function{
+        console.log("BOOM!")
+      }
+
+    }
+}
